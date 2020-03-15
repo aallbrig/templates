4d3fa38ec2bc2093fe8aeec68c316ebde8f052af
aallbright <aallbrig@gmail.com>
2020-03-15 10:26:11 -0400

    Add base config for ansible and add in the symlink shell script

---
+++ ansible/hacks/symlink-to-ansible-home.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# TODO: Implement
+# if roles directory exists, symlink all roles to ~/.ansible/roles
+
+# if tasks directory exists, symlink all tasks to ~/.ansible/tasks
+
+# if playbooks directory exists, symlink all playbooks to ~/.ansible/playbooks
+
+# if inventory directory exists, symlink all inventory to ~/.ansible/inventory
